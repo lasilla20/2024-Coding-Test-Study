9313c604512102692377f03023f134adbab89132
Jeong Chami <lasilla20@naver.com>
2024-11-22 20:24:18 +0900

    241122 통과

--- src/main/java/org/example/programmers/programmers_120814.java
+++ src/main/java/org/example/programmers/programmers_120814.java
@@ -3,7 +3,7 @@ package org.example.programmers;
 public class programmers_120814 {
     public int programmers_120814(int n) {
         if(n<0||n>100) return 0;
-        int answer = 1;
+        int answer=1;
         if(n<8) return 1;
         if(n%7>0) {
             answer = n/7+1;
--- src/main/java/org/example/programmers/programmers_120817.java
+++ src/main/java/org/example/programmers/programmers_120817.java
@@ -6,7 +6,7 @@ import java.util.stream.Stream;
 
 public class programmers_120817 {
     public double programmers_120817(int[] numbers) {
-        Integer[] i = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
+        Integer[] i =Arrays.stream(numbers).boxed().toArray(Integer[]::new);
         List<Integer> list = Arrays.asList(i);
         return list.stream().mapToInt(Integer::intValue).average().orElse(0);
     }
