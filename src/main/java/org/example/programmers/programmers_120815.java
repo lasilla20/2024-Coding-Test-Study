a3bb63abcf7f4923cacefa357f662fab0d7a3f37
Jeong Chami <lasilla20@naver.com>
2024-12-03 18:57:42 +0900

    241203 성공

--- src/main/java/org/example/programmers/programmers_120815.java
+++ src/main/java/org/example/programmers/programmers_120815.java
@@ -2,9 +2,9 @@ package org.example.programmers;
 
 public class programmers_120815 {
     public int programmers_120815(int n) {
-        if(n<3||n==6) return 1;
-        for(int i=n-1; i>1; i--){
-            if(((i*6)%n)==0) return i;
+        if(n<4||n==6) return 1;
+        else for(int i=1; i<100; i++){
+            if ((6*i)%n==0) return i;
         }
         return 1;
     }
--- src/main/java/org/example/programmers/programmers_120816.java
+++ src/main/java/org/example/programmers/programmers_120816.java
@@ -2,6 +2,12 @@ package org.example.programmers;
 
 public class programmers_120816 {
     public int programmers_120816(int slice, int n) {
-        return 0;
+        if (slice == n) return 1;
+        int pizza = 1;
+        while (pizza<100){
+            if(pizza*slice>=n) break;
+            pizza++;
+        }
+        return pizza;
     }
 }
