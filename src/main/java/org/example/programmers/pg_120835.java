1c81e9a115ebba26ba349cefe096de56937029ab
Jeong Chami <lasilla20@naver.com>
2024-12-06 21:09:44 +0900

    241206 실패

---
+++ src/main/java/org/example/programmers/pg_120835.java
@@ -0,0 +1,22 @@
+package org.example.programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class pg_120835 {
+    public int[] pg_120835(int[] emergency) {
+        int l = emergency.length;
+        int[] arr = Arrays.stream(l).sorted().toArray();
+        for (int i = 0; i < l; i++) {
+
+
+        }
+        int n=arr[0];
+        while(arr[em.length]>n){
+            em[]
+
+        }
+
+        return ;
+    }
+}
