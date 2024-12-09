5b92939804a1240fd6f5f79194447e57a5b7d339
Jeong Chami <lasilla20@naver.com>
2024-12-09 23:43:59 +0900

    241209 성공

---
+++ src/main/java/org/example/programmers/pg_120842.java
@@ -0,0 +1,16 @@
+package org.example.programmers;
+
+public class pg_120842 {
+    class Solution {
+        public int[][] solution(int[] num_list, int n) {
+            int[][] answer = new int[num_list.length][num_list.length];
+            for (int i = 0; i < num_list.length; i++) {
+                for (int j = 0; j < n; j++) {
+                    
+                }
+
+            }
+            return answer;
+        }
+    }
+}
