9d628f1aa08f05fd1f7c538fc65f21808dbfc169
Jeong Chami <lasilla20@naver.com>
2024-12-12 16:20:07 +0900

    241212 성공

--- src/main/java/org/example/programmers/pg_120844.java
+++ src/main/java/org/example/programmers/pg_120844.java
@@ -1,48 +1,21 @@
 package org.example.programmers;
 
 public class pg_120844 {
-    public int[] solution(int[] numbers, String direction) {
-        int tep = 0;
-        if (direction=="left"){
-            tep = numbers[0];
-            for(int i=0; i<numbers.length-1; i++){
-                numbers[i]=numbers[i+1];
-            }
-            numbers[numbers.length-1] = tep;
-        }
-        else {
-            tep = numbers[numbers.length-1];
-            for(int i=0; i<numbers.length-1; i++){
-                numbers[i+1]=numbers[i];
-            }
-            numbers[0] = tep;
-        }
-        return numbers;
-        }
-}
-
-class Solution {
-    public int[] solution(int[] numbers, String direction) {
-        int tep = 0;
+    public int[] pg_120844(int[] numbers, String d) {
+        int[] ans = new int[numbers.length];
         int len = numbers.length-1;
-        if (direction=="left"){
-            tep = numbers[0];
-            int i =0;
-            while(i<len){
-                numbers[i+1]=numbers[i];
-                i++;
+        if(d.equals("right")){
+            for (int i = 0; i < len; i++) {
+                ans[i+1] = numbers[i];
             }
-            numbers[len] = tep;
+            ans[0]=numbers[len];
         }
         else {
-            tep = numbers[len];
-            int i =0;
-            while(i<len){
-                numbers[i]=numbers[i+1];
-                i++;
+            for (int i = 1; i < numbers.length; i++) {
+                ans[i-1] = numbers[i];
             }
-            numbers[0] = tep;
+            ans[len] = numbers[0];
         }
-        return numbers;
+        return ans;
     }
 }
--- src/main/java/org/example/programmers/pg_120845.java
+++ src/main/java/org/example/programmers/pg_120845.java
@@ -1,7 +1,9 @@
 package org.example.programmers;
 
 public class pg_120845 {
-    public pg_120845() {
-
+    public int pg_120845(int[] box, int n) {
+        if (box[0]==1&&box[1]==1&&box[2]==1&&n==1) return 1;
+        if (box[0]<n||box[1]<n||box[2]<n) return 0;
+        return ((box[0]/n)*(box[1]/n)*(box[2]/n));
     }
 }
