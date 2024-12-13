219d59d6f46ff4dae4ae56346954e421d5c85965
Jeong Chami <lasilla20@naver.com>
2024-12-13 15:27:35 +0900

    241213 재풀이 필요

--- src/main/java/org/example/programmers/pg_120842.java
+++ src/main/java/org/example/programmers/pg_120842.java
@@ -3,12 +3,12 @@ package org.example.programmers;
 public class pg_120842 {
     class Solution {
         public int[][] solution(int[] num_list, int n) {
-            int[][] answer = new int[num_list.length][num_list.length];
-            for (int i = 0; i < num_list.length; i++) {
-                for (int j = 0; j < n; j++) {
-                    
+            int[][] answer = new int[num_list.length / n][n];
+            int cnt = 0;
+            for (int i = 0; i < answer.length; i++) {
+                for (int j = 0; j < answer[i].length; j++, cnt++) {
+                    answer[i][j] = num_list[cnt];
                 }
-
             }
             return answer;
         }
