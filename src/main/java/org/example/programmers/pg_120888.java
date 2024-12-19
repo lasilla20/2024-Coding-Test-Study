f59db1b15d7f142727e3552adf9065fdcab4fd1c
JEONG CHA MI <lasilla20@naver.com>
2024-12-19 13:32:13 +0900

    241218 성공
    241218 성공

---
+++ src/main/java/org/example/programmers/pg_120888.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string) {
+        String ans = my_string;
+        for (String s:my_string.split("")){
+            String temp = s;
+            ans = ans.replaceFirst(s,"1");
+            ans = ans.replaceAll(s,"");
+            ans = ans.replace("1",temp);
+        }
+    return ans;
+    }
+}
