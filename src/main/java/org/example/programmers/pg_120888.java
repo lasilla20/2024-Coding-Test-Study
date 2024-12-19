ed529d5f11e3f6ad6046ad89ca1d4e2dec20b92a
Jeong Chami <lasilla20@naver.com>
2024-12-19 16:38:56 +0900

    241219 수정

--- src/main/java/org/example/programmers/pg_120888.java
+++ src/main/java/org/example/programmers/pg_120888.java
@@ -1,4 +1,5 @@
-class Solution {
+package org.example.programmers;
+class pg_120888 {
     public String solution(String my_string) {
         String ans = my_string;
         for (String s:my_string.split("")){
--- src/main/java/org/example/programmers/pg_120889.java
+++ src/main/java/org/example/programmers/pg_120889.java
@@ -1,5 +1,6 @@
+package org.example.programmers;
 import java.util.*;
-class Solution {
+class pg_120889 {
     public int solution(int[] sides) {
         Arrays.sort(sides);
         return sides[0]+sides[1] > sides[2] ? 1 : 2;
